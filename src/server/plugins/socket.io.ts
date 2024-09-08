c61cbc99fe76f0940fd4ba4a5781151c71bde7b2
benni <benni.huff@gmail.com>
2024-09-08 19:09:25 +0200

    updated docker file

--- src/server/plugins/socket.io.ts
+++ src/server/plugins/socket.io.ts
@@ -181,13 +181,11 @@ export default defineNitroPlugin((nitroApp: NitroApp) => {
                 const nodeContext = peer.ctx.node;
                 const req = nodeContext.req;
 
-                // @ts-expect-error private method
                 engine.prepare(req);
 
                 const rawSocket = nodeContext.req.socket;
                 const websocket = nodeContext.ws;
 
-                // @ts-expect-error private method
                 engine.onWebSocket(req, rawSocket, websocket);
             }
         }
